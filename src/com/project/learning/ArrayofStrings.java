6677416ba1ec1741373e424a9bb451868e55dbd8
Gokul Jagadeesan <dhivyajagadeesan87559@gmail.com>
2022-01-18 10:11:51 +0530

    this is my Assisted_project

---
+++ src/com/project/learning/ArrayofStrings.java
@@ -0,0 +1,23 @@
+package com.project.learning;
+import java.util.Scanner;
+
+public class ArrayofStrings {
+    public static void main(String[] args) {
+        String[] arr={"Raju","Ram","Sam"};
+        System.out.println("Enter the string to search :");
+        Scanner sc =new Scanner(System.in);
+        String str=sc.next();
+        for (int i=0;i< arr.length;i++){
+            if (arr[i].equals(str)){
+                System.out.println("The item is present");
+                break;
+            }
+            else {
+                System.out.println("Not found");
+                break;
+            }
+        }
+    }
+}
+
+
